9a7808880063a68e385ac202b7a789cecd1927e8
林玥枫 <linyuefeng@codyy.com>
2017-07-07 16:14:38 +0800

    undo

--- js/index.js
+++ js/index.js
@@ -13,9 +13,10 @@
 //*****************************************************
 //*****************************************************
 //*****************************************************
-;(function(undefined) {
+; (function (undefined) {
 	var vm = window.vm || {},
 		trackCache = {},
+		reCache = [],
 		toString = Object.prototype.toString,
 		imgNode = document.createElement("IMG"),
 		ss = window.sessionStorage,
@@ -26,9 +27,9 @@
 			layout: "left-top-horizontal",
 			openLocalCache: true,
 			toolbars: ["line", "arrow", "pen", "circle", "ellipes", "circleStroke", "ellipesStroke", "rect", "rectStroke", "eraser", "floodFill", "eyeDropper", "text", "scissors", "import", "export", "clear", "undo", "color", "save"],
-			done: function() {}
+			done: function () { }
 		},
-		titles = {line:"直线", arrow:"箭头", pen:"画笔", circle:"实心圆", ellipes:"实心椭圆", circlestroke:"空心圆", ellipesstroke:"空心椭圆", rect:"实心矩形", rectstroke:"空心矩形", eraser:"橡皮擦", floodFill:"填充", eyeDropper:"取色工具", text:"文本", scissors:"截图", import:"插入图片", export:"导出", clear:"清除", undo:"撤销", color:"颜色", save:"保存"};
+		titles = { line: "直线", arrow: "箭头", pen: "画笔", circle: "实心圆", ellipes: "实心椭圆", circlestroke: "空心圆", ellipesstroke: "空心椭圆", rect: "实心矩形", rectstroke: "空心矩形", eraser: "橡皮擦", floodFill: "填充", eyeDropper: "取色工具", text: "文本", scissors: "截图", import: "插入图片", export: "导出", clear: "清除", undo: "撤销", color: "颜色", save: "保存" };
 
 	vm.module = vm.module || {};
 	window.vm = vm;
@@ -41,41 +42,41 @@
 				</div>";
 	var toolbarTpl = "<li class='toolbar-item-wrap'><i item='$ITEM$' class='iconfont icon-$CLASS$' title='$TITLE$'></i></li>";
 
-	var addEvent = window.addEventListener?function(target, type, fn, use) {
-		if(!target || !type || !fn) return ;
-		target.addEventListener(type, fn, use||false);
-	}:function(target, type, fn) {
-		if(!target || !type || !fn) return ;
-		target.attachEvent("on"+type, fn);
+	var addEvent = window.addEventListener ? function (target, type, fn, use) {
+		if (!target || !type || !fn) return;
+		target.addEventListener(type, fn, use || false);
+	} : function (target, type, fn) {
+		if (!target || !type || !fn) return;
+		target.attachEvent("on" + type, fn);
 	};
 
 	function WPad(params) {
-		if(!this instanceof WPad) return new WPad(params);
+		if (!this instanceof WPad) return new WPad(params);
 		this.name = params.name;
 		var self = this, _module = {};
-		this.getParams = function() { return params; };
-		this.getModule = function(name) { return !name?_module:_module[name]; };
-		this.getCache = function() { return trackCache[self.name]; };
+		this.getParams = function () { return params; };
+		this.getModule = function (name) { return !name ? _module : _module[name]; };
+		this.getCache = function () { return trackCache[self.name]; };
 		trackCache[self.name] = [];
-		buildPad.call(this, function(name, m) { _module[name] = m; });
+		buildPad.call(this, function (name, m) { _module[name] = m; });
 	}
 
 	WPad.prototype = {
 		constructor: WPad,
-		draw: function(data, isCache) {
-			if(!data) return;
+		draw: function (data, isCache) {
+			if (!data) return;
 			var self = this,
 				toolName = data.item.toLowerCase(),
 				tool = this.getModule(toolName);
 			tool && tool.draw.call(this, data);
-			if(-1!==["pen"].indexOf(toolName)) {
-				!isCache && trackCache[self.name].push(data);
+			if (-1 !== ["pen"].indexOf(toolName)) {
+				// !isCache && trackCache[self.name].push(data);
+				!isCache && "end" === data.pointType.toLowerCase() && trackCache[self.name].push(data);
 			} else {
-				!isCache && "end"===data.pointType.toLowerCase() && trackCache[self.name].push(data);
+				!isCache && "end" === data.pointType.toLowerCase() && trackCache[self.name].push(data);
 			}
-			console.log(trackCache)
-		},
-		clear: function() {
+		},	
+		clear: function () {
 			var self = this,
 				mainCanvas = this.getMainCanvas(),
 				mainCtx = mainCanvas.getContext("2d");
@@ -87,8 +88,8 @@
 	};
 
 	window.wPad = {
-		init: function(_params) {
-			if(!_params) return ;
+		init: function (_params) {
+			if (!_params) return;
 			var params = {};
 			copy(defaultConfig, params);
 			copy(_params, params);
@@ -106,29 +107,29 @@
 			str = "";
 
 		var i = 0, count = 0, len = toolbars.length;
-		if(len>i) {
+		if (len > i) {
 			do {
 				var toolbar = toolbars[i];
 
-				if(toolbar && vm.module[toolbar]) {
-					new vm.module[toolbar](function(m) {
-						str+=toolbarTpl.replace(/\$ITEM\$|\$CLASS\$/g, toolbar).replace(/\$TITLE\$/g, titles[toolbar]||"");
+				if (toolbar && vm.module[toolbar]) {
+					new vm.module[toolbar](function (m) {
+						str += toolbarTpl.replace(/\$ITEM\$|\$CLASS\$/g, toolbar).replace(/\$TITLE\$/g, titles[toolbar] || "");
 						fn.call(self, toolbar, m);
 						count++;
-						count>=len && "[object Function]"===toString.call(params.done) && params.done.call(self);
+						count >= len && "[object Function]" === toString.call(params.done) && params.done.call(self);
 					});
 				} else {
 					count++;
-					count>=len && "[object Function]"===toString.call(params.done) && params.done.call(self);
+					count >= len && "[object Function]" === toString.call(params.done) && params.done.call(self);
 				}
-			} while(++i<len)
+			} while (++i < len)
 		}
 
 		var padStr = tpl.replace(/\$TOOLBARS\$/g, str).replace(/\$LAYOUT\$/g, layout)
-						.replace(/\$MAINCANVASWIDTH\$/g, wrap.clientWidth)
-						.replace(/\$MAIINCANVASHEIGHT\$/g, wrap.clientHeight);
+			.replace(/\$MAINCANVASWIDTH\$/g, wrap.clientWidth)
+			.replace(/\$MAIINCANVASHEIGHT\$/g, wrap.clientHeight);
 		wrap.innerHTML = padStr;
-		
+
 		var toolbar = wrap.getElementsByClassName("toolbar-wrap")[0],
 			mainCanvas = wrap.getElementsByClassName("main-can")[0],
 			bufferCanvas = wrap.getElementsByClassName("buffer-can")[0],
@@ -137,64 +138,65 @@
 		mainCtx.fillStyle = "#fff";
 		mainCtx.fillRect(0, 0, mainCanvas.clientWidth, mainCanvas.clientHeight);
 
-			store.save(mainCanvas);
-		addEvent(mainCanvas, "mousedown", function() {
+		store.save(mainCanvas);
+		addEvent(mainCanvas, "mousedown", function () {
 			self.item.mousedown && self.item.mousedown.apply(self, [].slice.call(arguments, 0));
 		});
 
-		addEvent(mainCanvas, "mousemove", function() {
+		addEvent(mainCanvas, "mousemove", function () {
 			var e = arguments[0] || window.event;
-			1===e.which && self.item.mousemove && self.item.mousemove.apply(self, [].slice.call(arguments, 0));
+			1 === e.which && self.item.mousemove && self.item.mousemove.apply(self, [].slice.call(arguments, 0));
 		});
 
-		addEvent(mainCanvas, "mouseup", function() {
+		addEvent(mainCanvas, "mouseup", function () {
 			self.item.mouseup && self.item.mouseup.apply(self, [].slice.call(arguments, 0));
 		});
 
-		addEvent(toolbar, "click", function() {
+		addEvent(toolbar, "click", function () {
 			var e = arguments[0] || window.event,
 				item = e.srcElement.getAttribute("item") || e.target.getAttribute("item");
 
-			if(item) {
+			if (item) {
 				var tool = self.getModule()[item];
 
-				if(tool) {
+				if (tool) {
 					var name = tool.name || "";
-					if(!name) return ;
+					if (!name) return;
 
-					switch(name.toLowerCase()) {
-					case "clear":
-						self.clear();
-						params.clear.call(self);
-						break;
-					case "export":
-						tool.export.call(self);
-						break;
-					case "undo":
-						store.undo(mainCanvas.getContext('2d'));
-						break;
-					default:
-						self.item = tool;
-						mainCanvas.setAttribute("item", name);
-						ss.setItem(self.name, name.toLowerCase());
+					switch (name.toLowerCase()) {
+						case "clear":
+							self.clear();
+							params.clear.call(self);
+							break;
+						case "export":
+							tool.export.call(self);
+							break;
+						case "undo":
+							// store.undo(mainCanvas.getContext('2d'));
+							handleData(trackCache, self);
+							break;
+						default:
+							self.item = tool;
+							mainCanvas.setAttribute("item", name);
+							ss.setItem(self.name, name.toLowerCase());
 					}
-				}				
+				}
 			}
 
 			e.preventDefault();
 			e.stopPropagation();
 		});
 
-		self.getMainCanvas = function(){
+		self.getMainCanvas = function () {
 			return mainCanvas;
 		};
 
-		self.getBufferCanvas = function() {
+		self.getBufferCanvas = function () {
 			return bufferCanvas;
 		};
 
 		var tool = self.getModule()[item];
-		if(tool) {
+		if (tool) {
 			self.item = tool;
 			mainCanvas.setAttribute("item", tool.name);
 		}
@@ -202,34 +204,34 @@
 		var _trackCache = JSON.parse(ls.getItem(self.name) || "[]");
 		trackCache[self.name] = _trackCache;
 		var i = 0, len = _trackCache.length;
-		do { 
+		do {
 			var data = _trackCache[i];
-			data && self.draw(data, true); 
-		} while(++i<len);
+			data && self.draw(data, true);
+		} while (++i < len);
 	}
 
 	function copy(copyData, receiveData) {
-		if("[object Array]"===toString.call(copyData)) {
+		if ("[object Array]" === toString.call(copyData)) {
 			var i = 0, len = copyData.length;
-			if(len>i) {
+			if (len > i) {
 				do {
 					var d = copyData[i];
 
-					if("[object Array]"===toString.call(d) || "[object Object]"===toString.call(d)) {
-						var data = "[object Array]"===toString.call(d)?[]:{};
+					if ("[object Array]" === toString.call(d) || "[object Object]" === toString.call(d)) {
+						var data = "[object Array]" === toString.call(d) ? [] : {};
 						copy(d, data);
 						receiveData.push(data);
 					} else {
 						receiveData.push(d);
 					}
-				} while(++i<len)
+				} while (++i < len)
 			}
 		} else {
-			for(var key in copyData) {
+			for (var key in copyData) {
 				var d = copyData[key];
 
-				if("[object Array]"===toString.call(d) || "[object Object]"===toString.call(d)) {
-					var data = "[object Array]"===toString.call(d)?[]:{};
+				if ("[object Array]" === toString.call(d) || "[object Object]" === toString.call(d)) {
+					var data = "[object Array]" === toString.call(d) ? [] : {};
 					copy(d, data);
 					receiveData[key] = data;
 				} else {
@@ -239,8 +241,27 @@
 		}
 	}
 
-	addEvent(window, "beforeunload", function() {
-		for(var padName in trackCache) {
+	function handleData(data, self) {
+		var pad2 = data.pad1;
+		if (pad2.length) {
+			reCache.push(pad2.pop());
+			var mainCanvas = self.getMainCanvas(),
+				mainCtx = mainCanvas.getContext("2d");
+			mainCanvas.width = mainCanvas.width;
+			mainCtx.fillStyle = "#fff";
+			mainCtx.fillRect(0, 0, mainCanvas.clientWidth, mainCanvas.clientHeight);
+			if (pad2.length) {
+				pad2.map(function (item, index) {
+					var name = self.getModule(item.item.toLowerCase());
+					name.draw.call(self, item)
+				});
+			}
+
+		}
+	}
+
+	addEvent(window, "beforeunload", function () {
+		for (var padName in trackCache) {
 			ls.setItem(padName, JSON.stringify(trackCache[padName]));
 		}
 	});
--- js/pen.js
+++ js/pen.js
@@ -13,16 +13,16 @@
 //*****************************************************
 //*****************************************************
 //*****************************************************
-;(function() {
+; (function () {
 	function Pen(fn) {
-		if(!this instanceof Pen) return new Pen(fn);
+		if (!this instanceof Pen) return new Pen(fn);
 		this.name = "Pen";
 		fn && fn.call(Object.create(null), this);
 	}
 
 	Pen.prototype = {
 		constructor: Pen,
-		mousedown: function() {
+		mousedown: function () {
 			var e = arguments[0] || window.event,
 				params = this.getParams(),
 				mainCanvas = this.getMainCanvas(),
@@ -31,12 +31,12 @@
 
 			rect.top = rect.top + window.scrollY;
 			rect.left = rect.left + window.scrollX;
-			var x = e.clientX-rect.left, y = e.clientY-rect.top;
+			var x = e.clientX - rect.left, y = e.clientY - rect.top;
 			ctx.moveTo(x, y);
-			params.draw.call(this, {item:this.item.name, pointType:"begin", data:[x, y], width:mainCanvas.width, height:mainCanvas.height, time:Date.now()});
+			params.draw.call(this, { item: this.item.name, pointType: "begin", data: [x, y], width: mainCanvas.width, height: mainCanvas.height, time: Date.now() });
 		},
-		mousemove: function() {
-			var	e = arguments[0] || window.event,
+		mousemove: function () {
+			var e = arguments[0] || window.event,
 				params = this.getParams(),
 				mainCanvas = this.getMainCanvas(),
 				ctx = mainCanvas.getContext("2d"),
@@ -44,13 +44,13 @@
 
 			rect.top = rect.top + window.scrollY;
 			rect.left = rect.left + window.scrollX;
-			var	x = e.clientX-rect.left, y = e.clientY-rect.top;
+			var x = e.clientX - rect.left, y = e.clientY - rect.top;
 			ctx.lineTo(x, y);
 			ctx.stroke();
-			params.draw.call(this, {item:this.item.name, pointType:"join", data:[x, y], width:mainCanvas.width, height:mainCanvas.height, time:Date.now()});
+			params.draw.call(this, { item: this.item.name, pointType: "join", data: [x, y], width: mainCanvas.width, height: mainCanvas.height, time: Date.now() });
 		},
-		mouseup: function() {
-			var	e = arguments[0] || window.event, 
+		mouseup: function () {
+			var e = arguments[0] || window.event,
 				params = this.getParams(),
 				mainCanvas = this.getMainCanvas(),
 				ctx = mainCanvas.getContext("2d"),
@@ -58,29 +58,29 @@
 
 			rect.top = rect.top + window.scrollY;
 			rect.left = rect.left + window.scrollX;
-			var x = e.clientX-rect.left, y = e.clientY-rect.top;
-			params.draw.call(this, {item:this.item.name, pointType:"end", data:[x, y], width:mainCanvas.width, height:mainCanvas.height, time:Date.now()});
+			var x = e.clientX - rect.left, y = e.clientY - rect.top;
+			params.draw.call(this, { item: this.item.name, pointType: "end", data: [x, y], width: mainCanvas.width, height: mainCanvas.height, time: Date.now() });
 			ctx.save();
 		},
-		draw: function(data) {
+		draw: function (data) {
 			var pointType = data.pointType,
 				mainCanvas = this.getMainCanvas(),
 				ctx = mainCanvas.getContext("2d"),
-				xs = mainCanvas.width/data.width,
-				ys = mainCanvas.height/data.height,
+				xs = mainCanvas.width / data.width,
+				ys = mainCanvas.height / data.height,
 				data = data.data;
 
-			switch(pointType) {
-			case "begin":
-				ctx.moveTo((data[0]*xs)>>0, (data[1]*ys)>>0);
-			break;
-			case "join":
-				ctx.lineTo((data[0]*xs)>>0, (data[1]*ys)>>0);
-				ctx.stroke();
-			break;
-			default:
-				ctx.save();
-				store.save(mainCanvas)
+			switch (pointType) {
+				case "begin":
+					ctx.moveTo((data[0] * xs) >> 0, (data[1] * ys) >> 0);
+					break;
+				case "join":
+					ctx.lineTo((data[0] * xs) >> 0, (data[1] * ys) >> 0);
+					ctx.stroke();
+					break;
+				default:
+					ctx.save();
+					store.save(mainCanvas)
 			}
 		}
 	};
@@ -88,4 +88,4 @@
 	window.vm = window.vm || {};
 	window.vm.module = window.vm.module || {};
 	window.vm.module["pen"] = Pen;
-}(void(0)));
+}(void (0)));
